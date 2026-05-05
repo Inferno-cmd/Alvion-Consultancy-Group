594567b4da115b5d117f955f525ca4ea2ec8ce98
Inferno-cmd <blaccgene@gmail.com>
2026-05-05 09:52:23 +0300

    feature update hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                 <h1>The Future of Business Technology</h1>
                 <p>Discover the power of our cutting-edge technology solutions designed to propel your business into the future. A Value-Added Reseller (VAR) built to consult, sell,
 install, and maintain enterprise technology - end to end. we provide the tools you need to stay ahead in a rapidly evolving digital landscape.</p>
-                <button className='btn'>Learn More <img src={dark_arrow} alt="Learn More"/></button>
+                <button className='btn'>Learn More </button>
             </div>
         </div>
     )
