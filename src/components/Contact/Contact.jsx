164b8f2781908332758a4075542f01d0680c6cf6
Inferno-cmd <blaccgene@gmail.com>
2026-06-08 14:33:22 +0300

    updated contact component to use formspree for handling form submissions and added error handling for form validation. Also updated package.json to include formspree dependency.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-import './Contact.css';
+import './contact.css';
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
 
 const Contact = () => {
