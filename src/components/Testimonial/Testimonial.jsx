b83d75b6d0fced94d9dd0afbc2220106ae5192ff
Inferno-cmd <blaccgene@gmail.com>
2026-06-11 09:45:20 +0300

    edits

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -154,7 +154,7 @@ const Testimonial = () => {
                   <label>Your Feedback *</label>
                   <textarea 
                     rows="4" 
-                    placeholder="How has NexaTech helped scale or secure your systems architecture?" 
+                    placeholder="How has Alvion Consultancy Group helped scale or secure your systems architecture?" 
                     value={formData.feedback}
                     onChange={(e) => setFormData({...formData, feedback: e.target.value})}
                     required
